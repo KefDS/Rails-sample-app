1915540940e27269126dffeac8e1438716171dae
Kevin Delgado <kefdelgado@gmail.com>
2015-07-27 15:14:06 -0600

    Session added. (The session is avaible until the browser close)

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    # Información necesaria para logearse
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user and user.authenticate(params[:session][:password])
+      # Log the user in and redirect to the user's show page.
+      # sessions es una función de rails que viene con el módulo sessionsHelper
+      log_in(user)
+      redirect_to user_url(user)
+    else
+      # Now funciona para que aparezca solo una vez en página con render
+      flash.now[:danger] = "Invalid email/password combination"
+      render 'new'
+    end
+  end
+
+  def delete
+  end
+
+end
---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,15 @@
+module SessionsHelper
+	def log_in(user)
+		session[:user_id] = user.id
+	end
+
+	def current_user
+		@current_user ||= User.find_by(id: session[:user_id])
+	end
+
+	# Returns true if the user is logged in, false otherwise.
+	def logged_in?
+		!current_user.nil?
+	end
+
+end
