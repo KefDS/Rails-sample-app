d50c7d8878066f030a099fe21b3bd53050df25e5
Kevin Delgado <kefdelgado@gmail.com>
2015-08-05 14:37:42 -0600

    Finish user edit, update, index and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   # Se ejecuta antes de llamar a todos los métodos o métodos descritos en only:
-  before_action :logged_in_user, only:  [:index, :edit, :update]
+  before_action :logged_in_user, only:  [:index, :edit, :update, :destroy]
   before_action :correct_user, only:    [:edit, :update]
+  before_action :admin_user, only:      [:destroy]
 
   def index
     #@users = User.all
@@ -42,6 +43,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
   private
     def user_params
       # Strong params
@@ -63,6 +70,11 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+
+    # Confirms admin user
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
     end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,15 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create!(name: "Example User",
-						email: "example@railstutorial.org",
-						password: "foobar",
-						password_confirmation: "foobar")
+User.create!( name: "Example User",
+							email: "example@railstutorial.org",
+							password: "foobar",
+							password_confirmation: "foobar",
+							admin: true )
 
-User.create!(name: "Kevin Delgado",
-						 email: "kefdelgado@gmail.com",
-						 password: "foobar",
-						 password_confirmation: "foobar")
+User.create!( name: "Kevin Delgado",
+						  email: "kefdelgado@gmail.com",
+						  password: "foobar",
+						  password_confirmation: "foobar" )
 
 #Fake User
 99.times do |n|
